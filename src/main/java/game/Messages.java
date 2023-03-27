cad52310808cdb391eeed831307e0dfdcc01dc53
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2023-03-28 00:40:33 +0300

    2.9  changed picture matrix_man

--- src/main/java/game/Messages.java
+++ src/main/java/game/Messages.java
@@ -43,5 +43,5 @@ public class Messages {
 
     public static final String ENTER_NUMBER = "<html><center>Попытка №%s введи число:</center></html>";
 
-    public static final String IMAGINE = "/matrix man.png";
+    public static final String IMAGINE = "/matrix_man.jpg";
 }
